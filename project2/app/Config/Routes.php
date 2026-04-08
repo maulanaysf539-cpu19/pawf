52d4223b54e293bc9897fc225f9f3556be647769
Maulana Yusuf <maulanaysf539@gmai.com>
2026-04-08 23:13:21 +0700

    update 404 not found

--- project2/app/Config/Routes.php
+++ project2/app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/about', 'About::index');
+$routes->get('/resume','Resume::index');
+$routes->get('/services','Services::index');
+$routes->set404Override(function() {
+    echo view('errors/not_found');
+});
